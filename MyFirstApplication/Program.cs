ba4cc372b0e86a0f2817f5f1057c059fc2af9362
Newberrye <newberrydethan@gmail.com>
2022-03-22 22:01:31 -0500

    CAC-103 C# Overview

--- MyFirstApplication/Program.cs
+++ MyFirstApplication/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using MyFirstApplication;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+HouseExample();
+
+void HouseExample()
+{
+    House myHouse = new House();
+    myHouse.DoorOpenClose();
+
+}
