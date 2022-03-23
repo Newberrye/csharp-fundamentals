7bfa719417d58cd43b204c8262832e29002712d1
Newberrye <newberrydethan@gmail.com>
2022-03-23 11:07:06 -0500

    CAC-103 Lesson 1 Value Types

--- MyFirstApplication/Program.cs
+++ MyFirstApplication/Program.cs
@@ -1,21 +1,37 @@
 ﻿using MyFirstApplication;
 
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
 
 // The below statement calls a local function
 // single line comment
-HouseExample();
 
 /*
  Multi-line comment
-The below statement is a local function. Local functions are inside of members and are private by default. this means they are specific to that member and can only be called inside that member.
+The below statement is a local function. Local functions are inside of 
+members and are private by default. this means they are specific to 
+that member and can only be called inside that member.
  */
+
+// Lesson Examples
+// HouseExample();
+Lesson1Example();
+
 void HouseExample()
 {
     House myHouse = new House();
     myHouse.DoorOpenClose();
 
+}
+
+// Lesson 1 Value Types
+void Lesson1Example()
+{
+    Lesson1 myLesson = new Lesson1();
+    myLesson.IntMinMax();
+    myLesson.IntegralExample();
+    myLesson.ConvertFloatToInt();
+    myLesson.LongFromInt();
 }
 
 // ------------C# Exercise-------------
