a2d06d11722fc386d6d39f38b2b002e9cf913590
Newberrye <newberrydethan@gmail.com>
2022-03-22 23:10:50 -0500

    CAC-103 C# Exercise 1

--- MyFirstApplication/Program.cs
+++ MyFirstApplication/Program.cs
@@ -3,11 +3,31 @@
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+// The below statement calls a local function
+// single line comment
 HouseExample();
 
+/*
+ Multi-line comment
+The below statement is a local function. Local functions are inside of members and are private by default. this means they are specific to that member and can only be called inside that member.
+ */
 void HouseExample()
 {
     House myHouse = new House();
     myHouse.DoorOpenClose();
 
+}
+
+// ------------C# Exercise-------------
+
+/*
+This local function is  for my first exercise lab.
+*/
+
+void MyLocalFunction()
+{
+    Console.WriteLine("This is my first sentence");
+    Console.WriteLine("I am new to CSharp");
+    Console.WriteLine("This CSharp course is cool");
+    Console.WriteLine("I am learning stuff every day.");
 }
