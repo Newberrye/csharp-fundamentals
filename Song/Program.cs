9d333f9b45d6a9b88da74ea9fc35d457b6a9b363
Newberrye <newberrydethan@gmail.com>
2022-04-11 21:49:10 -0500

    CAC-103 Fixed some code, added comments

---
+++ Song/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Speech.Synthesis;
+namespace Song;
+
+/*
+ * Class which the program plays from.
+ */
+class Program
+{
+    /*
+     * Main 'method' which plays when program is ran.
+     */
+    static void Main()
+    {
+        // Object created using System.Speech library to talk.
+        SpeechSynthesizer singer = new SpeechSynthesizer();
+        singer.SetOutputToDefaultAudioDevice();
+        // Voice speed range slow to fast: -10 to 10
+        singer.Rate = 4;
+
+        int beerNum = 3;
+        string word = "bottles";
+        int voiceChanger = -1;
+
+        // Dictionary to hold voices
+        Dictionary<int, string> voices = new Dictionary<int, string>();
+        voices.Add(-1, "Microsoft David Desktop");
+        voices.Add(1, "Microsoft Zira Desktop");
+
+        while (beerNum > 0)
+        {
+            singer.Speak($"{beerNum} {word} of beer on the wall");
+            singer.Speak($"{beerNum} {word} of beer.");
+            singer.Speak("Take one down");
+            singer.Speak("Pass it around.");
+
+            --beerNum;
+
+            // Bottle change must be after increment change for next chorus to have correct wording
+            if (beerNum == 1)
+            {
+                word = "bottle";
+            }
+
+            if (beerNum > 0)
+            {
+                singer.Speak($"{beerNum} {word} of beer on the wall.");
+            }
+            else
+            {
+                singer.Rate = -10;
+                singer.Speak("No more bottles of beer on the wall.");
+            }
+
+            // Changes voice from male to female or vice-versa
+            voiceChanger *= -1;
+            singer.SelectVoice(voices[voiceChanger]);
+
+            // Slowest voice rate is -10
+            if (singer.Rate < -6)
+            {
+                singer.Rate -= 4;
+            }
+        }
+
+    }
+}
